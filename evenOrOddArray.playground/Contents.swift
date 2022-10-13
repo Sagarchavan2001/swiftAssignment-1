074f3e204897f45705c6a6f8eaf71ac901858046
Sagarchavan2001 <112470613+Sagarchavan2001@users.noreply.github.com>
2022-10-13 22:07:33 +0530

    Add files via upload

---
+++ evenOrOddArray.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+//check even odd in array
+var array:[Int]=[12,3,4,32,56,322,5]
+for i in array{
+    if i % 2 == 0
+    {
+        print("\(i) is even")
+    }
+    else{
+        print("\(i) is odd")
+    }
+}
